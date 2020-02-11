f545eca6970cc14e22d064da1da3a1edffa5d188
Jaji <aniljturan@gmail.com>
2020-02-11 17:43:45 +0300

    initial commit

---
+++ Sociable-Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sociable-Tic-Tac-Toe
+//
+//  Created by Sirius on 11.02.2020.
+//  Copyright © 2020 Jaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var player1Label: UILabel!
+    @IBOutlet weak var player2Label: UILabel!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sociable-Tic-Tac-ToeTests/Sociable_Tic_Tac_ToeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sociable_Tic_Tac_ToeTests.swift
+//  Sociable-Tic-Tac-ToeTests
+//
+//  Created by Sirius on 11.02.2020.
+//  Copyright © 2020 Jaji. All rights reserved.
+//
+
+import XCTest
+@testable import Sociable_Tic_Tac_Toe
+
+class Sociable_Tic_Tac_ToeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
